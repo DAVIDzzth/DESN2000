feb3408bcf82c2c82e583e0b807885f9094d14e6
jl03-d <leungjonathan0321@gmail.com>
2026-07-22 10:51:04 +1000

    light testing

--- Empty_Skeleton/Sources/main.c
+++ Empty_Skeleton/Sources/main.c
@@ -3,7 +3,34 @@
 #include "../LCD/lcd_hw.h"
 #include "../LCD/lcd_grph.h"
 
-int main() {
+
+
+void delay (void);
+
+int main(void) {
+	RCC->APB2ENR |= RCC_APB2ENR_IOPFEN;
+	
+	GPIOF->CRL = 0x22222222;
+	
+	while (1){
+		GPIOF->BSRR = 0x000000FF;
+		
+		delay();
+		
+		GPIOF->BSRR = 0x00FF0000;
+		
+		delay();
+
+		
+	}
 	
 	return 0;
 }
+
+void delay(void){
+	volatile unsigned int count;
+	
+	for(count = 0;count < 1000000; count++) {
+	}
+}
+
